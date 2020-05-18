3bc0a850eb87a4139a88fc7cc5bf9a02923019db
Jade Abbott <jabbott@retrorabbit.co.za>
2020-05-18 20:57:05 +0200

    Basics to train a simple lstm model, transformer script isn't working yet

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # joeynmt-starter
-A tutorial for JoeyNMT
+A starter for JoeyNMT
+
+## Installation
+
+Requires Python >=3.5. Run:
+`pip install joeynmt`
+
+## Run script
+
+`./train_lstm_model.sh`
--- train_lstm_model.sh
+++ train_lstm_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Train the transformer model using the config
-python3 -m joeynmt train config/transformer_enaf.yaml
+python3 -m joeynmt train config/lstm_enaf.yaml
 
 # Output the validation accuracy
-cat model/enaf_transformer/validations.txt
+cat model/enaf_lstm/validations.txt
 
 python3 -m joeynmt test "model/enaf_lstm/config.yaml"
